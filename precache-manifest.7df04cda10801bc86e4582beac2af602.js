ff3261f3edcd7bfde2d599f44f2a409151b2ac1b
Susmoy <susmoymukherjee@gmail.com>
2020-05-06 19:13:39 +0530

    Updates

---
+++ precache-manifest.7df04cda10801bc86e4582beac2af602.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "303ea8ee9b62489b86bf52255e6d528d",
+    "url": "/githubapp/index.html"
+  },
+  {
+    "revision": "3b33946bb49bbd18dd37",
+    "url": "/githubapp/static/css/2.a543c344.chunk.css"
+  },
+  {
+    "revision": "373c19679bf8ccbf8c1c",
+    "url": "/githubapp/static/css/main.5a280e57.chunk.css"
+  },
+  {
+    "revision": "3b33946bb49bbd18dd37",
+    "url": "/githubapp/static/js/2.00f90d1d.chunk.js"
+  },
+  {
+    "revision": "88a28bf9811004a7250f8b652924c27d",
+    "url": "/githubapp/static/js/2.00f90d1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "373c19679bf8ccbf8c1c",
+    "url": "/githubapp/static/js/main.03e764b2.chunk.js"
+  },
+  {
+    "revision": "6f9a97a342e942634a3e",
+    "url": "/githubapp/static/js/runtime-main.61de2c00.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/githubapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
