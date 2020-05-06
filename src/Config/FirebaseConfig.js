db4721e61499bf41b44c631cdce9d58f7a2642d1
SUSMOYOP <46573865+SUSMOY97@users.noreply.github.com>
2020-05-06 19:57:09 +0530

    Update FirebaseConfig.js

--- src/Config/FirebaseConfig.js
+++ src/Config/FirebaseConfig.js
@@ -1,10 +1,10 @@
 export default {
-    apiKey: "very funny! Hacker yeah ?",
-    authDomain: "oops sorry",
-    databaseURL: "Hahahaha",
-    projectId: "nahi bataunga",
-    storageBucket: "okay",
-    messagingSenderId: "hahahaha",
-    appId: "hahahaha",
-    measurementId: "hahahahah"
+    apiKey: "AIzaSyDVzxHVvZwKrIYsVGi4_klR9QRd6WuEnR0",
+    authDomain: "mygitapp-a96ae.firebaseapp.com",
+    databaseURL: "https://mygitapp-a96ae.firebaseio.com",
+    projectId: "mygitapp-a96ae",
+    storageBucket: "mygitapp-a96ae.appspot.com",
+    messagingSenderId: "876334747896",
+    appId: "1:876334747896:web:5808e10f4fc4140e1beb0d",
+    measurementId: "G-LY79D4CC3R"
   };
